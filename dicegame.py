391a3081579c7d84c87c11e71de2701ea13d3936
Nguyen Trung <nguyentrung01ute@gmail.com>
2025-11-23 21:44:42 +0700

    update game dice file

--- dicegame.py
+++ dicegame.py
@@ -1,4 +1,6 @@
 import random
+from random import choice
+
 # print("\u25CF\u250C\u2500\u2510\u2502\u2514\u2518")
 #● ┌ ─ ┐│└ ┘
 
@@ -40,23 +42,28 @@ dice_art = {
         "│ ●     ● │",
         "└─────────┘"),
 }
-dice = []
-total = 0
-numbers_of_dice = int(input("How many dice do you want? "))
 
-for die in range(numbers_of_dice):
-    dice.append(random.randint(1, 6))
+while True:
+    dice = []
+    total = 0
+    numbers_of_dice = int(input("How many dice do you want? "))
 
-# for die in range(numbers_of_dice):
-#     for line in dice_art.get(dice[die]):
-#         print(line)
+    for die in range(numbers_of_dice):
+        dice.append(random.randint(1, 6))
 
-for line in range(5):
-    for die in dice:
-        print(dice_art.get(die)[line], end="")
-    print()
+    # for die in range(numbers_of_dice):
+    #     for line in dice_art.get(dice[die]):
+    #         print(line)
 
-for dice in dice:
-    total += dice
+    for line in range(5):
+        for die in dice:
+            print(dice_art.get(die)[line], end="")
+        print()
 
-print(f"total {total}")
+    for dice in dice:
+        total += dice
+    print(f"total {total}")
+    choice = input("Press any key to continue...(y/n): ").lower().strip()
+    if choice != "y": break
+
+
