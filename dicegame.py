6afd03c5b3e002f96eaecd47e5b1b5a6f2820272
Nguyen Trung <nguyentrung01ute@gmail.com>
2025-11-24 15:36:09 +0700

    update

--- dicegame.py
+++ dicegame.py
@@ -43,10 +43,16 @@ dice_art = {
         "└─────────┘"),
 }
 
+debt1 = ["vay tien", "nap them tien", "hoan tat"]
+debt = 0
+count_debt = 0
 def PaytoWin(money) :
     tiennap = int(input("ban muon nap bao nhieu: $"))
     print(f"so du moi cua ban la: {tiennap + money}")
     return tiennap + money
+def Debt(money, vay) :
+    money += vay
+    return money
 
 money = 0
 print("pay your money to me")
@@ -76,11 +82,11 @@ while True:
 
     for dice in dices:
         total += dice
+    nha_cai_choose = "xiu"
     if total > 10:
         nha_cai_choose = "tai"
-    else:
-        nha_cai_choose = "xiu"
 
+    # xử lý phaafn in tiền và tính tiền
     print(f"total {total} -> {nha_cai_choose}")
     if (choose_tai_xiu == nha_cai_choose):
         print(f"Chuc mung ban! -> ban nhan duoc {tien_cuoc}$ vao tai khoan")
@@ -90,9 +96,43 @@ while True:
         print(f"xin chia buon cung ban! tai khoan cua ban -{tien_cuoc}$")
         money -= tien_cuoc
         print(f"tien cua ban hien tai la {money}")
-
-
     choice = input("Press any key to continue...(y/n): ").lower().strip()
-    if choice != "y": break
+    if choice != "y":
+        print("ban co muon thanh toan so tien ma ban da no? ")
+        print("1. yes")
+        print("2. no")
 
+        pay = int(input("Choose your option: "))
+        if pay == 1:
+
+            print(f"so tien con lai cua ban {money}")
+            pay_1 = int(input("nhap so tien ma ban muon tra: $"))
+            print("thanh toan thanh cong!")
+            money -= pay_1
+            debt -= pay_1
+        break
+
+    # vay de choi
+    if money <= 0:
+        print("Tai khoan cua ban khong du de tiep tuc choi")
+        print("some options: ")
+        for i in range(3):
+            print(f"{i + 1}. {debt1[i]}")
+
+        while True:
+            lc = int(input("Choose your option: "))
+            if lc == 1:
+                v = int(input("nhap so tien ma ban muon vay: $"))
+                money = Debt(money, v)
+                debt += v
+                count_debt += 1
+            elif lc == 2:
+                money = PaytoWin(money)
+            else:
+                break
+
+
+print("thong tin cua player sau khi choi: ")
+print(f"so du con lai cua ban la: ${money}")
+print(f"so tien con vay la ${debt}")
 
